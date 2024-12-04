3ae6e6fb9f255da6f7142df71b7ee3820b5d9cad
M78-ULTRA <aditrahmanto@gmail.com>
2024-12-04 14:16:11 +0700

    FE New Updates.

--- apps/web/src/components/navbar/index.tsx
+++ apps/web/src/components/navbar/index.tsx
@@ -4,14 +4,12 @@ const Navbar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-white dark:bg-black shadow-lg z-50 h-[8vh]">
       <div className="flex items-center h-full px-4 w-full">
-        {/* Left-aligned "FRUG" with bold font */}
         <div className="absolute left-4 flex items-center">
           <li className="font-bold text-2xl text-gray-800 dark:text-gray-200 list-none ml-4">
             FRUG
           </li>
         </div>
 
-        {/* Center-aligned other navbar links */}
         <div className="flex justify-center items-center space-x-8 mx-auto">
           <ul className="flex space-x-8 list-none">
             <li className="text-gray-800 dark:text-gray-200">Home</li>
