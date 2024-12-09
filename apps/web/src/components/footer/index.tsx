6fe476c7e5bd0687ab7c4ec6423e677c260ac44c
M78-ULTRA <aditrahmanto@gmail.com>
2024-12-09 13:21:32 +0700

    removed w-full.

--- apps/web/src/components/footer/index.tsx
+++ apps/web/src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
     return (
         <footer className="bg-black text-white h-[25vh]">
-            <div className="flex items-center h-full w-full ml-4">
+            <div className="flex items-center h-full ml-4">
                 <p className="font-bold text-4xl ml-4">Hop hop goes the FRUGGER.</p>
             </div>
         </footer>
